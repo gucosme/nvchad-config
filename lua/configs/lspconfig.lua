5d180e110769903ebb59bad0f6b32ff623dd791b
Gustavo Cosme <gucosme@users.noreply.github.com>
2024-12-18 14:16:37 -0300

    Add ts and eslint lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,44 @@
+-- read :h vim.lsp.config for changing options of lsp servers 
+
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local servers = { "html", "cssls", "ts_ls", "eslint" }
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local cwd = vim.fn.getcwd()
+local lsputils = require "lspconfig.util"
+local root_dir = lsputils.root_pattern ".yarn"(cwd)
+local default_config = {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
+
+vim.lsp.config.ts_ls = cwd:match "web%-code" and {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  init_options = {
+    maxTsServerMemory = 16384,
+    tsserver = {
+      path = root_dir .. "/.yarn/sdks/typescript/lib/tsserver.js",
+    },
+  },
+} or default_config
+
+vim.lsp.config.eslint = cwd:match "web%-code" and {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+    nvlsp.on_attach(client, bufnr)
+  end,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  workingDirectory = { mode = "auto" },
+  settings = {
+    nodePath = root_dir .. "/.yarn/sdks",
+  },
+} or default_config
+
+vim.lsp.enable(servers)
